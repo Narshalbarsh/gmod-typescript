d7c79ef711047ae44262eb09969f71e6eb3c5f56
Narshalbarsh <161986590+Narshalbarsh@users.noreply.github.com>
2025-10-18 15:27:42 -0400

    Fix cb for real this time

--- overrides/namespace/hook/Add.d.ts
+++ overrides/namespace/hook/Add.d.ts
@@ -1,9 +1,9 @@
 declare function Add<
-  N extends HookName,
-  A extends any[] = HookArgsFor<N>
+    N extends HookName,
+    A extends any[] = HookArgsFor<N>
 >(
-  name: N,
-  id: string,
-  cb: ((...args: A) => any) &
-      (Equals<A, HookArgsFor<N>> extends true ? unknown : never)
+    name: N,
+    id: string,
+    cb: ((...args: A) => any) &
+            (Equals<A, HookArgsFor<N>> extends true ? unknown : never)
 ): any;
